a53e186a156675cc9f7a1cb70538fb937bcf9543
Joval Junior <jovaljunior@gmail.com>
2023-09-03 17:06:13 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import { createServer } from "http";
+
+const PORT = process.env.PORT || 8080;
+const server = createServer((_, response) => {
+  response.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+
+  response.write(
+    JSON.stringify({ mensagem: "Hello world" })
+  );
+
+  response.end();
+});
+
+server.listen(PORT, () => console.log(`Listening ${PORT}`));
